9ab113d7384db6a7e0264218adabf12ccfe85573
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2024-05-04 12:20:38 +0200

    Added the home page
    The home page of the app and the main entry point to run
    the app. Only accessible after the user has signed in.

---
+++ streamlit_passwordless_app/main.py
@@ -0,0 +1,51 @@
+r"""The home page of the app and the main entry point to run the app."""
+
+# Standard library
+
+# Third party
+import streamlit as st
+import streamlit_passwordless as stp
+
+# Local
+from streamlit_passwordless_app.config import (
+    APP_HOME_PAGE_URL,
+    APP_ISSUES_PAGE_URL,
+    MAINTAINER_INFO,
+    STP_GITHUB_URL,
+)
+from streamlit_passwordless_app.controllers.home import controller
+
+ABOUT = f"""**The [streamlit-passwordless]({STP_GITHUB_URL}) showcase app**
+
+The streamlit-passwordless package provides a user model for Streamlit applications based
+on the Bitwarden passwordless technology. It allows users to securely register and sign in
+to a Streamlit application using the passkey FIDO2 and WebAuthn protocols.
+
+*The project is under development and not ready for production yet.*
+"""
+
+ABOUT_PAGE_CONFIG = f'{ABOUT}\n{MAINTAINER_INFO}'
+
+
+def app() -> None:
+    r"""The main function to run the home page of the app."""
+
+    st.set_page_config(
+        page_title='Streamlit Passwordless',
+        page_icon=':key:',
+        layout='centered',
+        menu_items={
+            'Get Help': APP_HOME_PAGE_URL,
+            'Report a bug': APP_ISSUES_PAGE_URL,
+            'About': ABOUT_PAGE_CONFIG,
+        },
+    )
+
+    if not st.session_state:
+        stp.init_session_state()
+
+    controller(about=ABOUT)
+
+
+if __name__ == "__main__":
+    app()
