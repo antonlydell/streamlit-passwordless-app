02b2b21eb690428c602b0ed0fc7d55a69ed449b1
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2024-05-04 12:22:01 +0200

    Added views for the register and sign in page

---
+++ streamlit_passwordless_app/views/register_and_sign_in.py
@@ -0,0 +1,19 @@
+r"""The views of the register and sign in page."""
+
+# Standard library
+
+# Third party
+import streamlit as st
+import streamlit_passwordless as stp
+
+# Local
+
+
+def register_and_sign_in_section(client: stp.BitwardenPasswordlessClient) -> None:
+    r"""Render the forms to register and sign in to the application."""
+
+    register_tab, signin_in_tab = st.tabs(["Register", "Sign in"])
+    with register_tab:
+        stp.bitwarden_register_form(client=client)
+    with signin_in_tab:
+        stp.bitwarden_sign_in_form(client=client)
