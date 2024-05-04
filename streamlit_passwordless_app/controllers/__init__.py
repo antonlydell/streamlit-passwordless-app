ac1a8722c0e0aca6cf60b6a1e02649867f529d74
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2024-05-04 12:17:21 +0200

    Added sub-package controllers
    Contains the controllers that manage the views and logic of the pages.
    Added the home page controller, which will render the home page.

---
+++ streamlit_passwordless_app/controllers/__init__.py
@@ -0,0 +1,1 @@
+r"""Contains the controllers that manage the views and logic of the pages."""
---
+++ streamlit_passwordless_app/controllers/home.py
@@ -0,0 +1,31 @@
+r"""The page controller of the home page."""
+
+# Standard library
+
+# Third party
+
+# Local
+from streamlit_passwordless_app.auth import is_authenticated
+from streamlit_passwordless_app.components.sidebars import sidebar
+from streamlit_passwordless_app.views.home import (
+    streamlit_passwordless_info_section,
+    title,
+    user_details_list,
+)
+
+
+def controller(about: str) -> None:
+    r"""Render the home page.
+
+    Parameters
+    ----------
+    about : str
+        The text about the streamlit-passwordless library to render
+        in the streamlit-passwordless info section.
+    """
+
+    authenticated, user = is_authenticated(redirect=True)
+    sidebar(is_authenticated=authenticated)
+    title()
+    streamlit_passwordless_info_section(text=about)
+    user_details_list(user=user)
