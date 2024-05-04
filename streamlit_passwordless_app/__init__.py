4ae5cd056e55112c7c9cc51626719073a376256b
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2024-05-04 11:34:20 +0200

    Added config module of package streamlit_passwordless_app
    streamlit_passwordless_app is the package that contains the app.
    The config module contains the configuration of the app.

---
+++ streamlit_passwordless_app/__init__.py
@@ -0,0 +1,16 @@
+r"""A Streamlit application to showcase the streamlit-passwordless library"""
+
+# Standard library
+from datetime import date
+
+__versiontuple__ = (1, 0, 0)
+r"""The version of streamlit-passwordless-app in a comparable form.
+Adheres to `Semantic Versioning <https://semver.org/spec/v2.0.0.html>`_
+(MAJOR.MINOR.PATCH).
+"""
+
+__version__ = "1.0.0"
+r"""The streamlit-passwordless-app version string."""
+
+__releasedate__ = date(2024, 5, 4)
+r"""The release date of the version specified in `__versiontuple__`."""
---
+++ streamlit_passwordless_app/config.py
@@ -0,0 +1,79 @@
+r"""Contains the configuration of the app."""
+
+# Standard library
+import logging
+from datetime import timedelta
+from enum import StrEnum
+
+# Third party
+import streamlit as st
+import streamlit_passwordless as stp
+
+# Local
+from streamlit_passwordless_app import __releasedate__, __version__
+
+logger = logging.getLogger(__name__)
+
+
+MAINTAINER_INFO = f"""\
+- Maintainer   : [*Anton Lydell*](https://github.com/antonlydell)
+- Version      : {__version__}
+- Release date : {__releasedate__}"""
+
+
+# =====================================================================================
+# URL:s
+# =====================================================================================
+
+APP_HOME_PAGE_URL = 'https://github.com/antonlydell/streamlit-passwordless-app'
+APP_ISSUES_PAGE_URL = 'https://github.com/antonlydell/streamlit-passwordless-app/issues'
+STP_GITHUB_URL = 'https://github.com/antonlydell/streamlit-passwordless'
+
+
+# =====================================================================================
+# Pages
+# =====================================================================================
+
+
+class Pages(StrEnum):
+    r"""The pages of the application."""
+
+    HOME = 'main.py'
+    REGISTER_AND_SIGN_IN = 'pages/register_and_sign_in.py'
+
+
+# =====================================================================================
+# Secrets
+# =====================================================================================
+
+STP_SECRETS_SECTION = 'streamlit-passwordless'
+STP_PUBLIC_KEY = 'STP_PUBLIC_KEY'
+STP_PRIVATE_KEY = 'STP_PRIVATE_KEY'
+
+
+# =====================================================================================
+# Functions
+# =====================================================================================
+
+
+@st.cache_resource(ttl=timedelta(days=7), show_spinner=False)
+def load_bitwarden_passwordless_credentials() -> tuple[str, str]:
+    r"""Load the public and private key for Bitwarden Passwordless.dev."""
+
+    public_key = st.secrets.get(STP_SECRETS_SECTION, {}).get(STP_PUBLIC_KEY)
+    private_key = st.secrets.get(STP_SECRETS_SECTION, {}).get(STP_PRIVATE_KEY)
+
+    if public_key is None or private_key is None:
+        error_msg = 'Bitwarden Passwordless credentials not found! Check your configuration.'
+        logger.error(error_msg)
+        stp.StreamlitPasswordlessError(error_msg)
+
+    return public_key, private_key
+
+
+@st.cache_resource(ttl=timedelta(days=7), show_spinner=False)
+def create_bitwarden_passwordless_client() -> stp.BitwardenPasswordlessClient:
+    r"""Create the client to communicate with Bitwarden Passwordless.dev."""
+
+    public_key, private_key = load_bitwarden_passwordless_credentials()
+    return stp.BitwardenPasswordlessClient(public_key=public_key, private_key=private_key)
